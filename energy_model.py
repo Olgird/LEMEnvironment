af1ba2770869a01ea0b355b46be77a204c78ce5a
Olgird <lic873@126.com>
2023-03-04 21:02:18 +0800

    add HVAC

--- energy_model.py
+++ energy_model.py
@@ -32,3 +32,40 @@ class ES():
         self.E_ES = E0_ES
 
         pass
+
+class HVAC():
+    """
+        init_indoor_temperature
+        outdoor_temperature
+        min_comfort_temperature
+        max_comfort_temperature
+        heat_capacity
+        heat_resistance
+        energy_efficiency
+        hvac_voltage
+        max_hvac_voltage
+    """
+    def __init__(self, init_indoor_temerpature, outtemp, min_comfort_temperature, max_comfort_temperature, heat_capacity,
+                 heat_resistance, energy_efficiency, max_hvac_voltage,
+                 timestep
+                 ):
+        self.init_indoor_temerpature = init_indoor_temerpature
+        self.min_comfort_temperature = min_comfort_temperature
+        self.max_comfort_temperature = max_comfort_temperature
+        self.heat_capacity = heat_capacity
+        self.outtemp = outtemp
+        self.heat_resistance = heat_resistance
+        self.energy_efficiency = energy_efficiency
+        self.max_hvac_voltage = max_hvac_voltage
+        self.timestep = timestep
+        self.now_temperature = self.init_indoor_tempature
+    
+    def calculate(self):
+        next_step_tempature = self.now_temperature - (self.now_temperature - self.outtemp + 
+        self.energy_efficiency * self.heat_resistance * self.heat_capacity) * self.timestep / (self.heat_capacity * self.heat_resistance)
+        return next_step_tempature
+    
+    """
+    thermal_cpmfort_weight is a const int
+    hvac_reward = -thermal_cpmfort_weight * (abs(now_tempature - max_temperature) + abs(min_temperature - now_temperature))
+    """

3a9c522f481ec030684c6ee9576935029d7dbbde
Olgird <lic873@126.com>
2023-03-05 15:27:48 +0800

    create configs

---
+++ configs.py
@@ -0,0 +1,32 @@
+
+# SA 的参数
+N_CYC_SA = 2
+P_CYC_SA = [0.56,0.63]
+
+# ES 的参数
+
+E_ES_CAPACITY = 10
+E_ES_MAX = 10
+E_ES_MIN = 2
+CHARGE_EFFICIENCY_ES = 0.95
+P_ES_MAX = 4
+
+# EV 的参数
+
+E_EV_CAPACITY = 15
+E_EV_MAX = 15
+E_EV_MIN = 12
+CHARGE_EFFICIENCY_EV = 0.93
+P_EV_MAX = 6
+
+# prosumer 的参数
+TIN = [10 , 1 , [8,12]]
+TTER = [18 , 1 , [16,20]]
+T0IN = [21 , 1 , [19,24]]
+E0ES = [6 , 1 , [4,8]]
+TDEP = [9 , 1 , [7,11]]
+TARR = [18 , 1 , [16,20]]
+E0EV = [9 , 1 , [6,12]]
+ECOM = [7.12 , 0.712 , [5.696 ,8.544]]
+
+
